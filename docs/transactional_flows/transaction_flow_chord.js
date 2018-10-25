656da60e5ac810195a1ebb07f827a2cdc12db8ba
Alexandre Popoff <al.popoff@free.fr>
2018-10-25 19:54:19 +0200

    Transactional flows bug fix

--- docs/transactional_flows/transaction_flow_chord.js
+++ docs/transactional_flows/transaction_flow_chord.js
@@ -23,7 +23,7 @@ var FLOW_MATRIX = [];
 var GOODS=[];
 var YEAR_DATA=[];
 
-d3.json("/static/all_merch.json", function(error, json_data) {
+d3.json("/ur3-viz/static/transaction_flow_data.json", function(error, json_data) {
 	if (error) throw error;
 
 	MERCH_DATA = json_data.qties;
